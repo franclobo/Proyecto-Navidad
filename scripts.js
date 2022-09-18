8f175ac14aebc0f25eb200805530cdd913558964
franclobo <fjbl2788@gmail.com>
2022-09-18 14:29:47 -0500

    Create PopUp window

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ const optMenu = document.querySelector('.mobile-menu-navigation');
 const dropDown = document.querySelector('.dropdown-menu');
 const scrollLock = document.querySelector('.content');
 const ourMembers = document.getElementById('members');
+const seeProjectButton = document.querySelectorAll('.pjbutton');
+const projectPopup = document.querySelector('.popup-window');
 
 mobileMenu.addEventListener('click', () => {
   dropDown.classList.remove('hide-menu');
@@ -85,3 +87,29 @@ ourMembers.innerHTML = `
     <div class="guide-bar-members"></div>
         ${seeMembers()}
         </div>`;
+
+function displayProject() {
+  projectPopup.classList.remove('hidePopup');
+  projectPopup.innerHTML = `
+    <div class="popup-container">
+      <div class="popup-header">
+        <button class="close-popup" onclick="projectPopup.classList.add('hidePopup');"><img src="./Icons/cross.svg" alt=""></button>
+        <h3 class="popup-title">Colabora con Nosotros</h3>
+      </div>
+      <div class="popup-content">
+        <p class="popup-p">
+          Agradecemos tu contribución económica al numero de cuenta:
+          <br/> Banco Pichincha
+          <br/> Cuenta de ahorros
+          <br/> 2204188183
+          <br/> A nombre de: Joselyn Quispillo
+        </p>
+      </div>
+    </div>`;
+}
+
+seeProjectButton.forEach((div) => {
+  div.addEventListener('click', () => {
+    displayProject();
+  });
+});
